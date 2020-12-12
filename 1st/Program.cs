40c6298d9bef72ff702338ab016b5e03832c8180
Wergiliy <75119828+Wergiliy@users.noreply.github.com>
2020-12-12 14:16:25 +1000

    Добавьте файлы проекта.

---
+++ 1st/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1st
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            float y = 5.5f;
+            decimal z = 5.5m;
+            Console.WriteLine(x);
+            Console.WriteLine(Math.PI);
+            Console.ReadKey();
+            
+        }
+    }
+}
